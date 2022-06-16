67fa9c84053f42ba55f997a3d0c4725e10bc2f4e
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2022-06-16 17:16:02 +0700

    add select data & year slider

--- myapp.py
+++ myapp.py
@@ -1,17 +1,13 @@
-import os
-
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import pandas as pd
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.models import BooleanFilter, CDSView, HoverTool, Range1d, Select
+from bokeh.models import BooleanFilter, CDSView, HoverTool, Select, Slider
 from bokeh.models.formatters import NumeralTickFormatter
-from bokeh.models.widgets import Dropdown
 from bokeh.palettes import Category20
 from bokeh.plotting import ColumnDataSource, figure
 
-# Define constants
-W_PLOT = 700
-H_PLOT = 400
+PLOT_WIDTH = 700
+PLOT_HEIGHT = 400
 TOOLS = 'pan,wheel_zoom,hover,reset'
 
 VBAR_WIDTH = 0.2
@@ -31,22 +27,33 @@ stock_select = Select(
     title='Stock name'
 )
 
-year_select = Select(
-    options=['2019', '2020', '2021'],
-    value='2019',
+year_slider = Slider(
+    start=2019,
+    end=2021, step=1, value=2019, 
     title='Year'
 )
 
 def get_symbol_df(symbol=None):
     df = pd.DataFrame(pd.read_csv('./dataset/Saham/' + symbol + '.csv'))
-    df["date"] = pd.to_datetime(df["date"])
-
-    year_start = year_select.value
-    year_end = int(year_start) + 1
-
-    df = df[(df['date'] >= f'{year_start}-07-29') & 
-        (df['date'] < f'{year_end}-01-01')]
     df.reset_index(inplace=True)
+
+    df['open_price'] = df['open_price'].astype(float)
+    df['close'] = df['close'].astype(float)
+    df['high'] = df['high'].astype(float)
+    df['low'] = df['low'].astype(float)
+    df['volume'] = df['volume'].astype(int)
+    df["date"] = pd.to_datetime(df["date"])
+    df.set_index('date')
+
+    year_start = year_slider.value
+    year_end = year_start + 1
+
+    if (year_start == 2021):
+        df = df[df['date'] >= f'{year_start}-01-01']
+    else:
+        df = df[(df['date'] >= f'{year_start}-01-01') & 
+            (df['date'] < f'{year_end}-01-01')]
+    
     return df
 
 source = ColumnDataSource(data={
@@ -60,9 +67,9 @@ source = ColumnDataSource(data={
 df = get_symbol_df('ARTO')
 source.data = source.from_df(df)
 
-plot = figure(plot_width=W_PLOT, plot_height=H_PLOT, tools=TOOLS,
+plot = figure(plot_width=PLOT_WIDTH, plot_height=PLOT_HEIGHT, tools=TOOLS,
                title=f"Harga saham {stock_select.value}", toolbar_location='above')
-print(f"source: {stock_select.value}")
+
 inc = source.data['close'] > source.data['open_price']
 dec = source.data['open_price'] > source.data['close']
 view_inc = CDSView(source=source, filters=[BooleanFilter(inc)])
@@ -71,8 +78,6 @@ view_dec = CDSView(source=source, filters=[BooleanFilter(dec)])
 plot.xaxis.major_label_overrides = {
     i+int(source.data['index'][0]): date.strftime('%b %d') for i, date in enumerate(pd.to_datetime(source.data["date"]))
 }
-plot.xaxis.bounds = (source.data['index'][0], source.data['index'][-1])
-
 
 plot.segment(x0='index', x1='index', y0='low', y1='high', color=RED, source=source, view=view_inc)
 plot.segment(x0='index', x1='index', y0='low', y1='high', color=GREEN, source=source, view=view_dec)
@@ -82,24 +87,15 @@ plot.vbar(x='index', width=VBAR_WIDTH, top='open_price', bottom='close', fill_co
 plot.vbar(x='index', width=VBAR_WIDTH, top='open_price', bottom='close', fill_color=RED, line_color=RED,
         source=source,view=view_dec, name="price")
 
-# plot.legend.location = "top_left"
-# plot.legend.border_line_alpha = 0
-# plot.legend.background_fill_alpha = 0
-# plot.legend.click_policy = "mute"
-
 plot.yaxis.formatter = NumeralTickFormatter(format='Rp 0,0[.]000')
 plot.x_range.range_padding = 0.05
 plot.xaxis.ticker.desired_num_ticks = 30
 plot.xaxis.major_label_orientation = 3.14/4
 
-# Select specific tool for the plot
 price_hover = plot.select(dict(type=HoverTool))
-
-# Choose, which glyphs are active by glyph name
 price_hover.names = ["price"]
-# Creating tooltips
 price_hover.tooltips = [("Datetime", "@date{%Y-%m-%d}"),
-                        ("Open", "@open_price{$0,0.00}"),
+                        ("Open", "@open_price{Rp0,0.00}"),
                         ("Close", "@close{Rp0,0.00}"),
                         ("Volume", "@volume{(Rp 0.00 a)}")]
 price_hover.formatters={"Date": 'datetime'}
@@ -109,27 +105,14 @@ def update_plot(attr, old, new):
     df = get_symbol_df(stock)
     new_data = source.from_df(df)
     source.data = new_data
+
+    plot.title.text = f"Harga saham {stock}"
     plot.xaxis.major_label_overrides = {
         i+int(source.data['index'][0]): date.strftime('%b %d') for i, date in enumerate(pd.to_datetime(source.data["date"]))
     }
-    plot.xaxis.bounds = (source.data['index'][0], source.data['index'][-1])
-
-
+    
 stock_select.on_change('value', update_plot)
-year_select.on_change('value', update_plot)
+year_slider.on_change('value', update_plot)
 
-# symbol = stock_select.value
-# df = get_symbol_df(symbol)
-# stock.data = stock.from_df(df)
-# elements = list()
-
-# # update_plot()
-# p_stock = update_plot(stock)
-
-# elements.append(p_stock)
-
-layout = row(column(stock_select, year_select), plot)
+layout = row(column(stock_select, year_slider), plot)
 curdoc().add_root(layout)
-
-# curdoc().add_root(column(elements))
-# curdoc().title = 'Bokeh stocks historical prices'
